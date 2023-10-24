d42464f84e3555ccb758fd3c12e2d573136f8a22
Brian-Wang0926 <aa0988402763@gmail.com>
2023-10-24 14:40:24 +0800

    push:first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+// import React, { useEffect, useState } from "react";
+import HomeComponent from "./components/home-component";
+
+function App() {
+
+  return (
+    <div className="App">
+      <HomeComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/home-component.js
@@ -0,0 +1,104 @@
+import React, { useEffect, useState, useRef } from "react";
+
+const HomeComponent = () => {
+  const [text, setText] = useState("");
+  const [image, setImage] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const fileInputRef = useRef(null);
+  const textInputRef = useRef(null);
+
+  const handleText = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleImage = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  const fetchData = async () => {
+    const response = await fetch("/api/getAllPosts");
+    const data = await response.json();
+    console.log("前端取得資料", data);
+    setPosts(data);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("text", text);
+    formData.append("image", image);
+
+    const response = await fetch("/api/post", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (response.ok) {
+      console.log("ok");
+      // 提交成功直接執行取得資料
+      fetchData();
+      setText("");
+      setImage(null);
+      fileInputRef.current.value = "";
+      textInputRef.current.value = "";
+    } else {
+      console.log("not ok");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div style={{ margin: "100px" }}>
+      <form onSubmit={handleSubmit}>
+        <h2>發表一篇圖文</h2>
+        <br />
+        <div className="form-group">
+          <label>文字內容:</label>
+          <input
+            className="form-control"
+            type="text"
+            onChange={handleText}
+            ref={textInputRef}
+          ></input>
+        </div>
+        <br />
+        <div>
+          <label>圖片檔案:</label>
+          <input
+            className="form-control"
+            type="file"
+            onChange={handleImage}
+            ref={fileInputRef}
+          ></input>
+        </div>
+        <br />
+        <div className="form-group">
+          <button className="btn btn-primary btn-block" type="submit">
+            送出
+          </button>
+        </div>
+      </form>
+      <hr />
+      <div>
+        <h3>所有圖文</h3>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <p>{post.text}</p>
+            <img
+              src={post.image_url}
+              alt="Uploaded content"
+              style={{ maxHeight: "100px" }}
+            />
+            <hr />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomeComponent;
